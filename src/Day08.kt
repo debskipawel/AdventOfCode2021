d8e670e92b000c23a7d2e180c6d0db8180fcc070
Paweł Dębski <pawel.debski.99@gmail.com>
2021-12-08 15:20:17 +0100

    Refactored the solution for day 8

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,36 +1,24 @@
 import helpers.permutations
 import java.io.File
 
+val keyAlphabet: List<Char> = listOf('a', 'b', 'c', 'd', 'e', 'f', 'g')
+
 class DisplayDecoder(key: String) {
-    private val charMap: Map<Char, Char>
+    private val charMap: MutableMap<Char, Char> = mutableMapOf()
 
     init {
-        val map = mutableMapOf<Char, Char>()
         for (i in key.indices) {
-            map[key[i]] = "abcdefg"[i]
+            charMap[key[i]] = keyAlphabet[i]
         }
-        this.charMap = map
     }
 
-    private val numberCodes: Map<String, Int>
-
-    init {
-        this.numberCodes = HashMap()
-        this.numberCodes["abcefg"] = 0
-        this.numberCodes["cf"] = 1
-        this.numberCodes["acdeg"] = 2
-        this.numberCodes["acdfg"] = 3
-        this.numberCodes["bcdf"] = 4
-        this.numberCodes["abdfg"] = 5
-        this.numberCodes["abdefg"] = 6
-        this.numberCodes["acf"] = 7
-        this.numberCodes["abcdefg"] = 8
-        this.numberCodes["abcdfg"] = 9
-    }
+    private val numberCodes: MutableMap<String, Int> = mutableMapOf(
+        "abcefg" to 0, "cf" to 1, "acdeg" to 2, "acdfg" to 3, "bcdf" to 4,
+        "abdfg" to 5, "abdefg" to 6, "acf" to 7, "abcdefg" to 8, "abcdfg" to 9
+    )
 
     fun decode(value: String): Int? {
         val decoded = value.map { charMap[it] }.sortedBy { it }.joinToString("")
-
         return numberCodes[decoded]
     }
 
@@ -39,47 +27,35 @@ class DisplayDecoder(key: String) {
     }
 }
 
-class DisplayRead(private val input: List<String>, val output: List<String>) {
-    init {
-        this.input.sortedBy { it.length }
-        this.output.sortedBy { it.length }
-    }
-
+class DisplayRead(input: List<String>, val output: List<String>) {
     private val decoder: DisplayDecoder
 
     init {
         // brute-forcing the key for decoding inputs
-        val permutations = listOf('a', 'b', 'c', 'd', 'e', 'f', 'g').permutations()
-            .map { it.joinToString("") }.filter {
-                key -> run {
-                    val decoder = DisplayDecoder(key)
+        val permutations = keyAlphabet.permutations()
+            .map { it.joinToString("") }
+            .filter { key -> run {
+                val decoder = DisplayDecoder(key)
 
-                    input.forEach {
-                        if (!decoder.canDecode(it)) {
-                            return@filter false
-                        }
-                    }
-
-                    true
+                input.forEach {
+                    if (!decoder.canDecode(it)) { return@filter false }
                 }
-            }
+
+                true
+            } }
 
         this.decoder = DisplayDecoder(permutations.first())
     }
 
     fun getOutputValue(): Int {
-        var value = 0
-        output.forEach {
-            value = value * 10 + decoder.decode(it)!!
-        }
-
-        return value
+        return output.map { decoder.decode(it) }.joinToString("").toInt()
     }
 }
 
 fun main() {
     val inputFile = "inputs\\input_day8.txt"
     val bufferedReader = File(inputFile).bufferedReader()
+
     val displayReads = bufferedReader.readLines().map {
         val split = it.split(" | ")
         DisplayRead(split[0].split(' '), split[1].split(' '))
